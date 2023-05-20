fc6e148633a164c518d56cdf35088e1ea734d91f
Saurabh Singh <saurabhaneja098@gmail.com>
2023-05-20 14:24:58 +0530

    Updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 const morgan = require('morgan')
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const path = require('path');
 
 //dotenv config
 dotenv.config();
@@ -22,8 +23,17 @@ app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
+
+//static Files
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, './client/build/index/html'));
+})
+
 //listen port
 const port = process.env.PORT || 8080
 app.listen(port, () =>{
     console.log(`Server running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}` .bgCyan.white);
 });
+ 
